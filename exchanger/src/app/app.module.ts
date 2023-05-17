572a5baeb8c3c9af359c6daa2265d1c9cbe8e705
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2023-05-17 20:14:00 +0300

    design

--- exchanger/src/app/app.module.ts
+++ exchanger/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ExchangeComponent } from './components/exchange/exchange.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ExchangeComponent
   ],
   imports: [
     BrowserModule,
---
+++ exchanger/src/app/components/exchange/exchange.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exchange',
+  templateUrl: './exchange.component.html',
+  styleUrls: ['./exchange.component.scss']
+})
+export class ExchangeComponent {
+
+}
